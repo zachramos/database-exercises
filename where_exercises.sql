53b9b5d1d053309ac87830f6c2632e65e9fd011f
zacharyramos <zachary.ramos9@gmail.com>
2018-03-19 16:41:19 -0500

    completed where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,6 +3,15 @@ USE employees;
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+# Add a condition to the previous query to find everybody with those names who is also male
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+ AND gender = 'M';
 # --7330 rows
 SELECT emp_no, first_name, last_name
 FROM employees
@@ -10,7 +19,7 @@ WHERE last_name LIKE 'e%';
 # --135,214 rows
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date LIKE '199%';
 # --842 rows
 SELECT emp_no, first_name, last_name, birth_date
 FROM employees
@@ -19,3 +28,19 @@ WHERE birth_date LIKE '%12-25';
 SELECT emp_no, first_name, last_name, gender
 FROM employees
 WHERE last_name like '%q%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE last_name like 'e%e';
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25';
+
+SELECT *
+FROM employees
+WHERE last_name like '%q%' AND last_name NOT LIKE '%qu%';
