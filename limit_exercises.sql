99a0f58b9912850d86ae50f2c65d2eeb8c364be5
zacharyramos <zachary.ramos9@gmail.com>
2018-03-20 10:46:13 -0500

    completed limit and offset exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,19 @@
+USE employees;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name = last_name
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
